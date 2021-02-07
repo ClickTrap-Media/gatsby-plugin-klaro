61fa83048bf03f32b7000ba5138a8c1479e87809
Pascal Zarrad <P.Zarrad@outlook.de>
2021-02-07 22:55:49 +0100

    build: remove jest

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,6 @@ const { series, parallel } = require("gulp");
 // Import tasks
 const { clean } = require("./tasks/clean");
 const { lint } = require("./tasks/lint");
-const { jest, jestCoverage } = require("./tasks/jest");
 const { babel } = require("./tasks/babel");
 
 global.projectRoot = __dirname;
@@ -18,14 +17,10 @@ module.exports.lint = lint;
 // Basic compile workflow
 const compile = parallel(babel);
 module.exports.compile = compile;
-// Lint and test
-module.exports.test = parallel(lint, jest);
-// Lint, test and create coverage report
-module.exports.coverage = series(lint, jestCoverage);
 // Build task that cleans output and compiles
-module.exports.build = series(clean, lint, jest, compile);
+module.exports.build = series(clean, lint, compile);
 // Build, but with coverage
-module.exports.buildCoverage = series(clean, lint, jestCoverage, compile);
+module.exports.buildCoverage = series(clean, lint, compile);
 
 // Set build task as default task
 module.exports.default = module.exports.build;
